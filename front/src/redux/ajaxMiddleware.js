8623f0ddce8f4519a46e1cce09ec4fee58a3ba48
sherah <2359932265@qq.com>
2018-02-01 12:02:46 +0800

    中间件

--- front/src/redux/ajaxMiddleware.js
+++ front/src/redux/ajaxMiddleware.js
@@ -1,63 +1,44 @@
 import http from '../utils/reqAjax'
 
-export function ajaxMiddleware(api) {
-    return function (dispatch) {
-        return function (action) {
-            //   if(action){ 
-                const { types, url, method = 'get', params = {} } = action
-                // console.log(action,6666)
-                if (!url) {
-                    return dispatch(action)
-                }
-                api.dispatch({
-                    type: 'beforeRequest'
-                })
-                if (url) {
-                    const [a,b,c] = types
-                    http[method](url, params).then(res => {
-                    // console.log(res)
-                        
-                        api.dispatch({
-                            type:b,
-                            response: res
-                        })
-                    }).catch(error => {
-                    api.dispatch({
-                        type: 'requestError',
-                        error
+ // types:['beforeRequest','Requested','RequestedError'],
+export function ajaxMiddleWare(MiddleWareAPI){
+    return function(dispatch){
+        return function(action){
+            const {types, url, method = 'get', params} = action;        
+            if(!url){
+                return dispatch(action);
+            }
+            MiddleWareAPI.dispatch({
+                type: types[0],
+            })
+
+            if(method =='post'){
+                http[method]({url:url,params}).then(res => {
+                    MiddleWareAPI.dispatch({
+                        type:types[1],
+                        respones:res.data
+                    })
+                }).catch(error => {
+                    MiddleWareAPI.dispatch({
+                        type: types[2],
+                        error,
                     })
                 })
-              }
+            }else if(method == 'get'){
+                http[method]({url:url,params}).then(res => {
+                    MiddleWareAPI.dispatch({
+                        type:types[1],
+                        respones:res.data
+                    })
+                }).catch(error => {
+                    MiddleWareAPI.dispatch({
+                        type: types[2],
+                        error,
+                    })
+                })
+            }
         }
     }
 }
 
-
-// export function userMiddleWare(middlewareAPI){
-//     return function(dispatch){
-//         return function(action){
-//             const {types,yes,url, method = 'get', params} = action;
-//             console.log(action)
-//             if(yes == 1){
-//                 console.log('未登录')
-//                 return dispatch({
-//                     type:'login'
-//                 });
-//             }
-//             if(yes==2){
-//                 console.log('已登录')
-//                 http[method](url, params).then(res => {
-//                     middlewareAPI.dispatch({
-//                         type: 'logined',
-//                         repsonse: res,
-//                     })
-//                 }).catch(error => {
-//                     middlewareAPI.dispatch({
-//                         type: 'requestError',
-//                         error
-//                     })
-//                 })
-//             }
-//         }
-//     }
-// }
+                     
