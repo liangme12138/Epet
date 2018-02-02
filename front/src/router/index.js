bcfae6fda7e89f671e80c1acd5b8ff8b89582ec6
zcy <m18577363440@163.com>
2018-02-02 11:44:44 +0800

    首页

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Router, Route, Link, hashHistory, IndexRoute} from 'react-router';
-import goodslistComponent from "../components/goodList/goodsListComponent"
+import goodslistComponent from "../components/goodList/goodsListComponent";
+import IndexComponent from '../components/Index/indexComponent';
 
 export default (
         <div>
-            <Route path="/" component={goodslistComponent}>                
-            </Route>
+            <Route path="/" component={goodslistComponent}></Route>
+            <Route path="/index" component={IndexComponent}></Route>            
         </div>
 )
